22b2faa9116259c4b812870346f064195dc250bd
MKDS <mkarepov@gmail.com>
2024-10-07 00:00:00 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2758
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red184\green93\blue213;\red0\green0\blue0;\red0\green0\blue0;
+\red44\green155\blue54;}
+{\*\expandedcolortbl;;\cssrgb\c77647\c47059\c86667;\cssrgb\c0\c0\c0\c7843;\cssrgb\c0\c0\c0\c83137;
+\cssrgb\c19608\c65882\c27451;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+import\cf4  streamlit \cf2 as\cf4  st\
+\
+st.header(\cf5 'Tossing a Coin'\cf4 )\
+\
+st.write(\cf5 'It is not a functional application yet. Under construction.'\cf4 )}
